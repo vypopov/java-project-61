96207724f47a2ac9f7b15101f6fa7ba0aed1a613
Vladimir Popov <popovvladimir23@gmail.com>
2025-05-13 22:02:06 +0300

    fix

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,31 @@
 package hexlet.code;
 
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Player player = new Player("Start new game");
+        Scanner sc = new Scanner(System.in);
+        String startMsg = """
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                0 - Exit
+                """;
+        System.out.println(startMsg);
+        System.out.println("Your choice:");
+        var usrChose = sc.nextLine();
+        switch (usrChose) {
+            case "1":
+                Cli.start(player);
+                break;
+            case "2":
+                break;
+            default:
+                System.out.println("You must chose games(1-3) or quit(0)!");
+                break;
+        }
 
     }
 }
--- app/src/main/java/hexlet/code/Util.java
+++ app/src/main/java/hexlet/code/Util.java
@@ -1,4 +1,8 @@
 package hexlet.code;
 
+import java.util.Random;
 public class Util {
+    public static int getrandom(int min, int max) {
+        return 1;
+    }
 }
